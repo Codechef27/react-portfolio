2abdebe1f76b193a4823d893f480f2812e807fc8
BradleyK <bradleyjkennett@gmail.com>
2022-08-21 21:23:38 -0400

    hero sec. add

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section className='hero'>
+            <div>
+                <p>Hello, I'm <span className="#10d4a3f1">Bradley Kennett.</span></p>
+                <p>In the near future I will be a full-stack web developer.</p>
+                <a href="#portfolio">
+                    <button className="btn-1"> View My Work </button>
+                </a>
+            </div>
+        </section>
+    )
+}
+
+export default About
